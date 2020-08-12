9502d248475d75c596eb20633014eed515aece9a
Seth Wang <seth_wang@trend.com.tw>
2020-08-12 18:25:23 +0800

    refine folder structure

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except:
 
 # read the contents of your README file
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, '../', 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 if __name__ == '__main__':
