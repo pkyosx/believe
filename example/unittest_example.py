f073e5734ff975278e92171c9de4c348f181e6ab
Seth Wang <seth_wang@trendmicro.com>
2021-11-09 17:52:08 +0800

    Remove outdated doc

--- example/unittest_example.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from believe import BelieveMixin
-
-class BelieveTestCase(unittest.TestCase, BelieveMixin):
-    def test_believe(self):
-        self.assertEquals(self.AnyStr(), "abc")
-        self.assertEquals(self.AnyIntStr(), "1")
